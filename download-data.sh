7aee03dbf7157a2161af0031ecd394c241e65f0a
aaron777collins <aaron777collins@gmail.com>
2023-01-28 14:53:35 -0500

    fixed download and added another website link

--- download-data.sh
+++ download-data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Download data from the web
-curl https://data.transportation.gov/api/views/7qu7-j8rx/rows.csv?accessType=DOWNLOAD -o data/data.csv
+curl -L https://data.transportation.gov/api/views/7qu7-j8rx/rows.csv?accessType=DOWNLOAD -o data/data.csv
